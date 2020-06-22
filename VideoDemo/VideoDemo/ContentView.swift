d0e259b89993a557d6c7f1cd41758b606b27a233
neelam0806 <39704512+neelam0806@users.noreply.github.com>
2020-06-22 15:09:55 +0530

    Add files via upload
    Commit master branch

---
+++ VideoDemo/VideoDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VideoDemo
+//
+//  Created by Neelam Gupta on 22/06/20.
+//  Copyright © 2020 Neelam Gupta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
